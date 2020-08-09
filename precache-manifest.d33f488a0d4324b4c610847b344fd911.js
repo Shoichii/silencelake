680c67e34dc29f588b49bd27a34f850cece91841
Shoichii <khayn1992@gmail.com>
2020-08-09 23:00:39 +0300

    Updates

---
+++ precache-manifest.d33f488a0d4324b4c610847b344fd911.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd59373348cffa0b280dde1fc380968c",
+    "url": "/silencelake/index.html"
+  },
+  {
+    "revision": "913fa0ca21bb24056101",
+    "url": "/silencelake/static/css/main.5b13b301.chunk.css"
+  },
+  {
+    "revision": "31973cc34183667772e9",
+    "url": "/silencelake/static/js/2.31695d6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/silencelake/static/js/2.31695d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913fa0ca21bb24056101",
+    "url": "/silencelake/static/js/main.a16f6b5e.chunk.js"
+  },
+  {
+    "revision": "ac07d736d3bcea461765",
+    "url": "/silencelake/static/js/runtime-main.7a181aa1.js"
+  },
+  {
+    "revision": "6935b16779f54ff13527aabbb296ac1e",
+    "url": "/silencelake/static/media/PhotoOfAVastWheat Field.6935b167.jpg"
+  },
+  {
+    "revision": "127382652f4d8e6a74914928af93846e",
+    "url": "/silencelake/static/media/PhotoOfTranquilRiver.12738265.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.b0beb947902edee0cca365183098610e.js"
+  "/silencelake/precache-manifest.d33f488a0d4324b4c610847b344fd911.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/silencelake/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
